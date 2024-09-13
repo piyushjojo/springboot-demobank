fe721f9f650d1458935349f05aeed36ca8c2783f
piyushjojo <impiyushjoshi@gmail.com>
2024-09-14 03:22:26 +0530

    card dto updated to handle negative amounts

--- cards/src/main/java/com/demo/cards/dto/CardsDto.java
+++ cards/src/main/java/com/demo/cards/dto/CardsDto.java
@@ -3,6 +3,7 @@ package com.demo.cards.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
@@ -26,13 +27,16 @@ public class CardsDto {
 
     @NotEmpty(message = "mandatory field")
     @Schema(description = "total card limit")
+    @PositiveOrZero(message = "total limit should be zero or more")
     private int totalLimit;
 
     @NotEmpty(message = "mandatory field")
     @Schema(description = "total amount used")
+    @PositiveOrZero(message = "amount used should be zero or more")
     private int amountUsed;
 
     @NotEmpty(message = "mandatory field")
     @Schema(description = "remaining amount")
+    @PositiveOrZero(message = "available amount should be zero or more")
     private int availableAmount;
 }
